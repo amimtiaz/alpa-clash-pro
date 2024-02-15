8230d3a993215a21b8ea4fa2e4017917dc70a6d7
amimtiaz <amemtiaj19@gmail.com>
2024-02-15 18:07:32 +0600

    function added

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -1,4 +1,15 @@
-function paly() {
-    // hide the home screen
-    // show the playground
+// function paly() {
+//     // step-1 hide the home screen. to hide the screen add the class hidden to home section
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+
+
+//     //show the playground
+//     const playGround = document.getElementById('play-ground');
+//     playGround.classList.remove('hidden');
+// }
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
 }
