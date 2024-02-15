5974406da5304a69fde25a34a8b08d7e7c063cb2
amimtiaz <amemtiaj19@gmail.com>
2024-02-15 22:51:17 +0600

    finished

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -9,7 +9,23 @@
 //     playGround.classList.remove('hidden');
 // }
 
+function continueGame() {
+    // step -1: generate a random alphabet
+    const allpahbet = getRandomAlphabet();
+    console.log('your random alpahbet number:', allpahbet);
+
+    // set randomly generated alphabet to the screen
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = allpahbet;
+
+    // set the background color
+    setBackgroundColorById(allpahbet)
+
+}
+
 function play(){
     hideElementById('home-screen');
     showElementById('play-ground');
+    continueGame()
 }
+
--- script/utility.js
+++ script/utility.js
@@ -9,3 +9,27 @@ function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
+
+function setBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400')
+}
+
+function getRandomAlphabet() {
+    // get or create an alphabet array
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    
+    const alphabets = alphabetString.split('');
+    //console.log(alphabets);
+
+
+    // get a random allpahbet
+    const randomNumber = Math.random() * 25;
+    const index = Math.round(randomNumber);
+    
+
+    const alpahbet = alphabets[index];
+    //console.log(index, alpahbet);
+    return alpahbet;
+
+}
